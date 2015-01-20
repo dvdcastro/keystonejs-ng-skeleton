a32027450bbeb01b51de3d31454e9eb803eb6c31
David Castro <david@sofhouse.net>
2015-01-20 16:13:33 -0500

    Updated to use keystone 0.2.42

--- routes/index.js
+++ routes/index.js
@@ -48,40 +48,42 @@ exports = module.exports = function(app) {
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
-	app.get('/api/post/list', middleware.corsAPI, routes.api.posts.list);
-	app.all('/api/post/create', middleware.corsAPI, routes.api.posts.create);
-	app.get('/api/post/:slug', middleware.corsAPI, routes.api.posts.get);
-	app.all('/api/post/:id/update', middleware.corsAPI, routes.api.posts.update);
-	app.get('/api/post/:id/remove', middleware.corsAPI, routes.api.posts.remove);
+	// app.all('/api*', keystone.initCORS);
 
-	app.get('/api/post-category/list', middleware.corsAPI, routes.api.post_categories.list);
-	app.get('/api/post-category/:key', middleware.corsAPI, routes.api.post_categories.get);
+	app.get('/api/post/list', [keystone.initAPI, keystone.cors], routes.api.posts.list);
+	app.all('/api/post/create', [keystone.initAPI, keystone.cors], routes.api.posts.create);
+	app.get('/api/post/:slug', [keystone.initAPI, keystone.cors], routes.api.posts.get);
+	app.all('/api/post/:id/update', [keystone.initAPI, keystone.cors], routes.api.posts.update);
+	app.get('/api/post/:id/remove', [keystone.initAPI, keystone.cors], routes.api.posts.remove);
 
-	app.get('/api/post-by-category/:key', middleware.corsAPI, routes.api.post_by_category.list);
+	app.get('/api/post-category/list', [keystone.initAPI, keystone.cors], routes.api.post_categories.list);
+	app.get('/api/post-category/:key', [keystone.initAPI, keystone.cors], routes.api.post_categories.get);
 
-	app.get('/api/gallery/list', middleware.corsAPI, routes.api.galleries.list);
-	app.get('/api/gallery/:key', middleware.corsAPI, routes.api.galleries.get);
+	app.get('/api/post-by-category/:key', [keystone.initAPI, keystone.cors], routes.api.post_by_category.list);
 
-	app.get('/api/enquiry/list', middleware.corsAPI, routes.api.enquiries.list);
-	app.all('/api/enquiry/create', middleware.corsAPI, routes.api.enquiries.create);
-	app.all('/api/enquiry/:id/update', middleware.corsAPI, routes.api.enquiries.update);
-	app.get('/api/enquiry/:id/remove', middleware.corsAPI, routes.api.enquiries.remove);
+	app.get('/api/gallery/list', [keystone.initAPI, keystone.cors], routes.api.galleries.list);
+	app.get('/api/gallery/:key', [keystone.initAPI, keystone.cors], routes.api.galleries.get);
 
-	app.get('/api/enquiry_type/list', middleware.corsAPI, routes.api.enquiry_types.list);
-	app.get('/api/enquiry_type/:value', middleware.corsAPI, routes.api.enquiry_types.get);
+	app.get('/api/enquiry/list', [keystone.initAPI, keystone.cors], routes.api.enquiries.list);
+	app.all('/api/enquiry/create', [keystone.initAPI, keystone.cors], routes.api.enquiries.create);
+	app.all('/api/enquiry/:id/update', [keystone.initAPI, keystone.cors], routes.api.enquiries.update);
+	app.get('/api/enquiry/:id/remove', [keystone.initAPI, keystone.cors], routes.api.enquiries.remove);
+
+	app.get('/api/enquiry_type/list', [keystone.initAPI, keystone.cors], routes.api.enquiry_types.list);
+	app.get('/api/enquiry_type/:value', [keystone.initAPI, keystone.cors], routes.api.enquiry_types.get);
 	
 	// App Routes for Angular Material Project
-	app.get('/ang-mat', routes.ang_mat.app);
-	app.get('/ang-mat/blog', routes.ang_mat.blog);
-	app.get('/ang-mat/post', routes.ang_mat.post);
-	app.get('/ang-mat/gallery', routes.ang_mat.gallery);
-	app.get('/ang-mat/contact', routes.ang_mat.contact);
+	app.get('/ang-mat', [keystone.initAPI, keystone.cors], routes.ang_mat.app);
+	app.get('/ang-mat/blog', [keystone.initAPI, keystone.cors], routes.ang_mat.blog);
+	app.get('/ang-mat/post', [keystone.initAPI, keystone.cors], routes.ang_mat.post);
+	app.get('/ang-mat/gallery', [keystone.initAPI, keystone.cors], routes.ang_mat.gallery);
+	app.get('/ang-mat/contact', [keystone.initAPI, keystone.cors], routes.ang_mat.contact);
 
 	// App Routes for Angular Bootstrap Material Project
-	app.get('/ang_bootm', routes.ang_bootm.app);
-	app.get('/ang_bootm/blog', routes.ang_bootm.blog);
-	app.get('/ang_bootm/post', routes.ang_bootm.post);
-	app.get('/ang_bootm/gallery', routes.ang_bootm.gallery);
-	app.get('/ang_bootm/contact', routes.ang_bootm.contact);
+	app.get('/ang_bootm', [keystone.initAPI, keystone.cors], routes.ang_bootm.app);
+	app.get('/ang_bootm/blog', [keystone.initAPI, keystone.cors], routes.ang_bootm.blog);
+	app.get('/ang_bootm/post', [keystone.initAPI, keystone.cors], routes.ang_bootm.post);
+	app.get('/ang_bootm/gallery', [keystone.initAPI, keystone.cors], routes.ang_bootm.gallery);
+	app.get('/ang_bootm/contact', [keystone.initAPI, keystone.cors], routes.ang_bootm.contact);
 	
 };
